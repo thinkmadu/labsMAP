96570b3b1a0fe10c78e1e57a139321959bb26877
Madu <41195347+thinkmadu@users.noreply.github.com>
2024-04-19 20:57:09 -0300

    Create Main.java

---
+++ lab3/Main.java
@@ -0,0 +1,28 @@
+package lab3;
+
+import lab3.Funcoes.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Acervo collection = new Acervo();
+
+        Filme filme1 = collection.adicionarFilme("Avatar", "2009", "Action");
+        Filme filme2 = collection.adicionarFilme("Tropa de Elite", "2007", "Action");
+        Filme filme3 = collection.adicionarFilme("Avengers", "2012", "Action");
+
+        Funcionario funcionario1 = filme1.adicionarFuncionario("Douglas", "1234");
+        Funcionario funcionario2 = filme2.adicionarFuncionario("Fernanda", "12345");
+        Funcionario funcionario3 = filme3.adicionarFuncionario("Lucio", "123456");
+
+        funcionario1.setOcupacao(new Ator());
+        funcionario1.setOcupacao(new Roteirista());
+
+        funcionario2.setOcupacao((new Diretor()));
+
+        funcionario3.setOcupacao(new Ator());
+        funcionario3.setOcupacao(new Roteirista());
+        funcionario3.setOcupacao(new Diretor());
+
+        collection.mostrarFilmes();
+    }
+}
