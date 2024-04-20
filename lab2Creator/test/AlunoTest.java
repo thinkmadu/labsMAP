baf788361685dc4d37ac4928bbef6608dd0b3d0d
Madu <41195347+thinkmadu@users.noreply.github.com>
2024-04-19 23:26:13 -0300

    Adicionando arquivos de teste

--- lab2Creator/test/AlunoTest.java
+++ lab2Creator/test/AlunoTest.java
@@ -1,5 +1,61 @@
 package lab2Creator.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import lab2Creator.*;
+
 public class AlunoTest {
-    
+    private Aluno aluno1 = Creator.criarAluno("Paulo", "4545");
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testaGetNome() {
+        String nomeEsperado = "Paulo";
+        String nomeAtual = aluno1.getNome();
+
+        assertEquals(nomeEsperado, nomeAtual);
+    }
+
+    @Test
+    public void testaSetNome() {
+        String novoNome = "Ana";
+        aluno1.setNome(novoNome);
+
+        assertEquals(novoNome, aluno1.getNome());
+    }
+
+    @Test
+    public void testaGetMatricula() {
+        String matriculaEsperada = "4545";
+        String matriculaAtual = aluno1.getMatricula();
+
+        assertEquals(matriculaEsperada, matriculaAtual);
+    }
+
+    @Test
+    public void testaSetMatricula() {
+        String novaMatricula = "1234";
+        aluno1.setMatricula(novaMatricula);
+
+        assertEquals(novaMatricula, aluno1.getMatricula());
+    }
+
+    @Test
+    public void testaGetAlunoTurma() {
+        assertNotNull(aluno1.getAlunoTurma());
+        assertTrue(aluno1.getAlunoTurma() instanceof AlunoTurma);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testaGetAlunoTurmaNullPointerException() {
+        Aluno alunoNulo = null;
+        alunoNulo.getAlunoTurma();
+    }
 }
--- lab2Creator/test/ProfessorTest.java
+++ lab2Creator/test/ProfessorTest.java
@@ -1,5 +1,40 @@
 package lab2Creator.test;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import lab2Creator.*;
+
 public class ProfessorTest {
-    
+    private Professor professor;
+
+    @Before
+    public void setUp() {
+        // Criando professor usando o padrão Creator
+        professor = Creator.criarProfessor("Ana Carolina", "422222");
+    }
+
+    @Test
+    public void testaGetNome() {
+        String nomeEsperado = "Ana Carolina";
+        String nomeAtual = professor.getNome();
+
+        assertEquals(nomeEsperado, nomeAtual);
+    }
+
+    @Test
+    public void testaGetMatricula() {
+        String matriculaEsperada = "422222";
+        String matriculaAtual = professor.getMatricula();
+
+        assertEquals(matriculaEsperada, matriculaAtual);
+    }
+
+    @Test
+    public void testaTamanhoListaHorarios() {
+        int tamanhoEsperado = 0;
+        int tamanhoAtual = professor.getHorarios().getListaTurmas().size();
+
+        assertEquals(tamanhoEsperado, tamanhoAtual);
+    }
 }
